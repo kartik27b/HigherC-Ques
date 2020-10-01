b0a61a92d6ebd8db89c13e2c09e7fe08c7417f13
raghav2805 <59332635+raghav2805@users.noreply.github.com>
2020-10-01 13:44:01 +0530

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define fastio ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define fastio ios_base::sync_with_stdio(false);cin.tie(false);cout.tie(false)
 
 int main()
 {
