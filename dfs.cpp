0d9eaf0190adaa13428f6dafa85543ca63314f24
raghav2805 <59332635+raghav2805@users.noreply.github.com>
2020-10-01 13:40:19 +0530

    Fast cin,cout

--- dfs.cpp
+++ dfs.cpp
@@ -1,7 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+
 int main()
 {
+    fastio;
     int n;
     cin>>n;
     if(n%400==0)
